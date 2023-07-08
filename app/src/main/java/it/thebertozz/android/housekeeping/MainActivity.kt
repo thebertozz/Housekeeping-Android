f3673257db620085248991a6afe0f4e6b510c8bb
Federico Bertoli <federico@Bertos-MacBook-Pro.local>
2023-07-08 10:34:11 +0200

    updated dependencies and added firebase google-services.json file

--- app/src/main/java/it/thebertozz/android/housekeeping/MainActivity.kt
+++ app/src/main/java/it/thebertozz/android/housekeeping/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import it.thebertozz.android.housekeeping.ui.theme.HousekeepingTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,23 +15,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             HousekeepingTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //HousekeepingApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HousekeepingTheme {
-        Greeting("Android")
     }
 }
